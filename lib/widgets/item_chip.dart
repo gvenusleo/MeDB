f4559e1f5461b9c5c3e407ad43d704d475c5c6bc
liuyuxin <gvenusleo@gmail.com>
2025-04-21 23:41:51 +0800

    fix: item_chip 参数错误

--- lib/widgets/item_chip.dart
+++ lib/widgets/item_chip.dart
@@ -6,7 +6,7 @@ import 'package:medb/widgets/svg.dart';
 class ItemChip extends StatelessWidget {
   final String svg;
   final String label;
-  final String? value;
+  final dynamic value;
   final double fontSize;
 
   const ItemChip({
@@ -43,15 +43,44 @@ class ItemChip extends StatelessWidget {
         if (value != null) ...[
           const SizedBox(width: 4),
           Expanded(
-            child: Text(
-              svg == Svg.datetime ? dateTimeToString(value!) : value!,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontSize: fontSize,
-                color: Get.theme.colorScheme.outline,
-              ),
-            ),
+            child:
+                svg == Svg.hash
+                    ? SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          for (final e in value!)
+                            Container(
+                              decoration: BoxDecoration(
+                                color:
+                                    Get
+                                        .theme
+                                        .colorScheme
+                                        .surfaceContainerHighest,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              margin: EdgeInsets.only(right: 4),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 4,
+                                vertical: 2,
+                              ),
+                              child: Text(
+                                e.toString(),
+                                style: TextStyle(fontSize: fontSize - 2),
+                              ),
+                            ),
+                        ],
+                      ),
+                    )
+                    : Text(
+                      svg == Svg.datetime
+                          ? dateTimeToString(value!.toString())
+                          : value!.toString(),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: fontSize),
+                    ),
           ),
         ],
       ],
